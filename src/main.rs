057867d0fdc4e459da9f4fd568c9ce49bf297c26
Tiqur <trevorbrage02@gmail.com>
2023-05-16 02:44:18 -0700

    Setup basic CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1) {
+        Some(arg) => match arg.as_str() {
+            // Serve file to another client 
+            "serve" => {
+                println!("Serving...");
+            }
+            // Listen for incoming packets
+            "listen" => {
+                println!("Listening...");
+            }
+            _ => {
+
+            }
+        }
+        _ => {}
+    }
 }
